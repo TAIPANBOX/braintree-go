e144e7f7d4dde9459b22ae4db0dd6e58fe0fe970
Manuel Stofer <manuel@smallpdf.com>
2016-02-18 16:05:24 +0100

    Enable Addresses and Subscriptions in Customer type

--- customer.go
+++ customer.go
@@ -6,18 +6,20 @@ import (
 )
 
 type Customer struct {
-	XMLName      string       `xml:"customer"`
-	Id           string       `xml:"id,omitempty"`
-	FirstName    string       `xml:"first-name,omitempty"`
-	LastName     string       `xml:"last-name,omitempty"`
-	Company      string       `xml:"company,omitempty"`
-	Email        string       `xml:"email,omitempty"`
-	Phone        string       `xml:"phone,omitempty"`
-	Fax          string       `xml:"fax,omitempty"`
-	Website      string       `xml:"website,omitempty"`
-	CreditCard   *CreditCard  `xml:"credit-card,omitempty"`
-	CreditCards  *CreditCards `xml:"credit-cards,omitempty"`
-	CustomFields CustomFields `xml:"custom-fields"`
+	XMLName       string         `xml:"customer"`
+	Id            string         `xml:"id,omitempty"`
+	FirstName     string         `xml:"first-name,omitempty"`
+	LastName      string         `xml:"last-name,omitempty"`
+	Company       string         `xml:"company,omitempty"`
+	Email         string         `xml:"email,omitempty"`
+	Phone         string         `xml:"phone,omitempty"`
+	Fax           string         `xml:"fax,omitempty"`
+	Website       string         `xml:"website,omitempty"`
+	CreditCard    *CreditCard    `xml:"credit-card,omitempty"`
+	CreditCards   *CreditCards   `xml:"credit-cards,omitempty"`
+	Subscriptions *Subscriptions `xml:"subscriptions,omitempty"`
+	CustomFields  CustomFields   `xml:"custom-fields"`
+	Addresses     *Addresses     `xml:"addresses",omitempty`
 }
 
 // DefaultCreditCard returns the default credit card, or nil
